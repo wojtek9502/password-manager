2c1c45187098aead682589d8a62697baaedd572d
wojtek9502 <wojtek.klusek@gmail.com>
2024-02-17 00:03:50 +0100

    fix auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -18,10 +18,15 @@ def _is_token_valid_with_main_token(api_key) -> bool:
 
 async def validate_api_key(api_key: str = Security(api_key_header)):
     user_token_service = UserTokenService()
+    if not api_key:
+        raise HTTPException(status_code=401, detail='Invalid or missing API Key')
+
+    is_token_valid = _is_token_valid_with_main_token(api_key=api_key)
+    if is_token_valid:
+        return
 
     is_token_valid = user_token_service.is_token_valid(token=api_key)
-    if not is_token_valid:
-        is_token_valid = _is_token_valid_with_main_token(api_key=api_key)
+    if is_token_valid:
+        return
 
-    if not is_token_valid:
-        raise HTTPException(status_code=401, detail='Invalid or missing API Key')
+    raise HTTPException(status_code=401, detail='Invalid or missing API Key')
