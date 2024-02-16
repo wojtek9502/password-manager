12631c1a512d63aa3dde4662dcb5ba97341e7f1d
wojtek9502 <wojtek.klusek@gmail.com>
2024-02-17 00:17:29 +0100

    add const pepper to password encrypt and decrypt

--- src/password/cryptography.py
+++ src/password/cryptography.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
@@ -6,29 +7,37 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-backend = default_backend()
 
+# Source https://stackoverflow.com/a/55147077
 class CryptographyFernet:
     @staticmethod
     def _derive_key(password: bytes, salt: bytes, iterations: int) -> bytes:
         """Derive a secret key from a given password and salt"""
+        backend = default_backend()
         kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(), length=32, salt=salt,
             iterations=iterations, backend=backend)
         return b64e(kdf.derive(password))
 
     def password_encrypt(self, message: bytes, password: str, iterations: int) -> bytes:
+        pepper = os.environ['PASSWORD_ENCRYPT_PEPPER']
         salt = secrets.token_bytes(16)
+        password = password + pepper
+
         key = self._derive_key(password.encode(), salt, iterations)
+        fernet_encrypted = Fernet(key).encrypt(message)
         return b64e(
             b'%b%b%b' % (
                 salt,
                 iterations.to_bytes(4, 'big'),
-                b64d(Fernet(key).encrypt(message)),
+                b64d(fernet_encrypted),
             )
         )
 
     def password_decrypt(self, token: bytes, password: str) -> bytes:
+        pepper = os.environ['PASSWORD_ENCRYPT_PEPPER']
+        password = password + pepper
+
         decoded = b64d(token)
         salt, iterations, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
         iterations = int.from_bytes(iterations, 'big')
